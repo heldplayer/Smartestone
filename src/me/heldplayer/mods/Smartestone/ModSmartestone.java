3ed0068389b410f8d1512f7cf759384c5cc0bac4
Heldplayer <mitchel.pyl@gmail.com>
2013-06-18 13:33:11 +0200

    Update HeldCore, add support for modpacks and opting out of statistics gathering

--- src/me/heldplayer/mods/Smartestone/ModSmartestone.java
+++ src/me/heldplayer/mods/Smartestone/ModSmartestone.java
@@ -35,7 +35,6 @@ public class ModSmartestone {
     // HeldCore Objects
     private UsageReporter reporter;
     private Config config;
-    public static ConfigValue<Boolean> silentUpdates;
     public static ConfigValue<Integer> blockMulti1Id;
     public static ConfigValue<Integer> blockMulti2Id;
     public static ConfigValue<Integer> blockMicroId;
@@ -43,6 +42,10 @@ public class ModSmartestone {
     public static ConfigValue<Integer> itemMicroBlockId;
     public static ConfigValue<Integer> itemWaterCoreId;
     public static ConfigValue<Boolean> HDTextures;
+    // Config values for HeldCore
+    public static ConfigValue<Boolean> silentUpdates;
+    public static ConfigValue<Boolean> optOut;
+    public static ConfigValue<String> modPack;
 
     @PreInit
     public void preInit(FMLPreInitializationEvent event) {
@@ -54,10 +57,7 @@ public class ModSmartestone {
 
         Objects.log = event.getModLog();
 
-        this.reporter = new UsageReporter(Objects.MOD_ID, Objects.MOD_VERSION, FMLCommonHandler.instance().getSide(), file);
-
         // Config
-        silentUpdates = new ConfigValue<Boolean>("silentUpdates", Configuration.CATEGORY_GENERAL, null, Boolean.TRUE, "Set this to true to hide update messages in the main menu");
         blockMulti1Id = new ConfigValue<Integer>("MultiBlock1", Configuration.CATEGORY_BLOCK, null, 2100, "");
         blockMulti2Id = new ConfigValue<Integer>("MultiBlock2", Configuration.CATEGORY_BLOCK, null, 2101, "");
         blockMicroId = new ConfigValue<Integer>("MicroId", Configuration.CATEGORY_BLOCK, null, 2102, "");
@@ -65,8 +65,10 @@ public class ModSmartestone {
         itemMicroBlockId = new ConfigValue<Integer>("MicroBlockItem", Configuration.CATEGORY_ITEM, null, 5241, "");
         itemWaterCoreId = new ConfigValue<Integer>("WaterCore", Configuration.CATEGORY_ITEM, null, 5242, "");
         HDTextures = new ConfigValue<Boolean>("HD-Textures", Configuration.CATEGORY_GENERAL, Side.CLIENT, Boolean.FALSE, "");
-        this.config = new Config(event.getSuggestedConfigurationFile());
-        this.config.addConfigKey(silentUpdates);
+        silentUpdates = new ConfigValue<Boolean>("silentUpdates", Configuration.CATEGORY_GENERAL, null, Boolean.TRUE, "Set this to true to hide update messages in the main menu");
+        optOut = new ConfigValue<Boolean>("optOut", Configuration.CATEGORY_GENERAL, null, Boolean.FALSE, "Set this to true to opt-out from statistics gathering. If you are configuring this mod for a modpack, please leave it set to false");
+        modPack = new ConfigValue<String>("modPack", Configuration.CATEGORY_GENERAL, null, "", "If this mod is running in a modpack, please set this config value to the name of the modpack");
+        config = new Config(event.getSuggestedConfigurationFile());
         this.config.addConfigKey(blockMulti1Id);
         this.config.addConfigKey(blockMulti2Id);
         this.config.addConfigKey(blockMicroId);
@@ -74,8 +76,13 @@ public class ModSmartestone {
         this.config.addConfigKey(itemMicroBlockId);
         this.config.addConfigKey(itemWaterCoreId);
         this.config.addConfigKey(HDTextures);
+        this.config.addConfigKey(silentUpdates);
+        this.config.addConfigKey(optOut);
+        this.config.addConfigKey(modPack);
         this.config.load();
         this.config.saveOnChange();
+
+        this.reporter = new UsageReporter(Objects.MOD_ID, Objects.MOD_VERSION, modPack.getValue(), FMLCommonHandler.instance().getSide(), file);
 
         Updater.initializeUpdater(Objects.MOD_ID, Objects.MOD_VERSION, silentUpdates.getValue());
 
@@ -89,10 +96,12 @@ public class ModSmartestone {
 
     @PostInit
     public void postInit(FMLPostInitializationEvent event) {
-        Thread thread = new Thread(this.reporter, Objects.MOD_ID + " usage reporter");
-        thread.setDaemon(true);
-        thread.setPriority(Thread.MIN_PRIORITY);
-        thread.start();
+        if (optOut.getValue()) {
+            Thread thread = new Thread(this.reporter, Objects.MOD_ID + " usage reporter");
+            thread.setDaemon(true);
+            thread.setPriority(Thread.MIN_PRIORITY);
+            thread.start();
+        }
 
         proxy.postInit(event);
     }
